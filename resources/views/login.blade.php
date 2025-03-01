468e45290799249484f3e8553a8ec2aec2953f9b
Ade Indra Pratama <adeprathama@gmail.com>
2025-03-02 00:18:13 +0700

    Initial Commit and Slicing Login Page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,33 @@
+@extends("layout.layout")
+
+@section("content")
+
+    <div class="row height-width-100">
+
+        <div class="col-md-6 login-page-left">
+            <p class="plagcheck-login-logo">plagcheck</p>
+            <p class="plagcheck-subtitle">Scan Secure Succeed</p>
+        </div>
+
+        <div class="col-md-6 login-page-right">
+            <p class="hello-again-text">Hello Again!</p>
+            <p class="hello-again-subtitle">Login to Your Account</p>
+
+            <input type="text" placeholder="Enter to your email" id="email" name="email" class="login-input">
+            <input type="password" placeholder="Password" id="password" name="password" class="login-input">
+            <a href="#" class="forgot-password-link">Forgot Password?</a>
+            <button type="submit" class="login-button">Login</button>
+
+            <p class="footer-login-text">Or continue with</p>
+            <div class="google-facebook-button-container">
+                <button type="button" class="google-facebook-button">Google</button>
+                <button type="button" class="google-facebook-button">Facebook</button>
+            </div>
+
+            <p class="footer-login-text">Not registered? <a href="#" class="footer-login-link">Sign Up now!</a></p>
+
+        </div>
+
+    </div>
+
+@stop
